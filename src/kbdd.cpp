d903521c18b2107765cfa9e9e69dbb12cbc271f4
Oleksandr Litvinov <greenulito@gmail.com>
2021-10-04 11:34:57 +0300

    Forget view on unmapped signal

--- src/kbdd.cpp
+++ src/kbdd.cpp
@@ -2,7 +2,6 @@
 #include <xkbcommon/xkbcommon.h>
 #include <wayfire/plugin.hpp>
 #include <wayfire/output.hpp>
-#include <wayfire/util/log.hpp>
 #include <wayfire/signal-definitions.hpp>
 #include <wayfire/nonstd/wlroots-full.hpp>
 
@@ -17,17 +16,35 @@ class kbdd_plugin : public wf::plugin_interface_t
     const int default_layout_id = 0;
 
 
-    int get_layout(int view_id) {
+    void restore_view_layout(int view_id, wlr_keyboard *keyboard) {
+        int layout_id;
+
         if (views.find(view_id) == views.end()) {
-            return default_layout_id;
+            layout_id = default_layout_id;
+        } else {
+            layout_id = views.at(view_id);
         }
-        return views.at(view_id);
-    }
+
+        wlr_keyboard_notify_modifiers(keyboard,
+                                      keyboard->modifiers.depressed,
+                                      keyboard->modifiers.latched,
+                                      keyboard->modifiers.locked,
+                                      layout_id);
+    };
 
 
-    void put_layout(int view_id, int layout_id) {
-        views[view_id] = layout_id;
-    }
+    void save_prev_view_layout(wlr_keyboard *keyboard) {
+        if (prev_view_id > 0) {
+            int prev_layout_id = xkb_state_serialize_layout(keyboard->xkb_state,
+                                                            XKB_STATE_LAYOUT_LOCKED);
+            views[prev_view_id] = prev_layout_id;
+        }
+    };
+
+
+    void delete_view_layout(int view_id) {
+        views.erase(view_id);
+    };
 
 
     wf::signal_callback_t on_focus_changed = [=] (wf::signal_data_t *data)
@@ -38,23 +55,22 @@ class kbdd_plugin : public wf::plugin_interface_t
             return;
         }
 
-        if (prev_view_id) {
-            int prev_layout_id = xkb_state_serialize_layout(keyboard->xkb_state,
-                                                            XKB_STATE_LAYOUT_LOCKED);
-            put_layout(prev_view_id, prev_layout_id);
-        }
+        save_prev_view_layout(keyboard);
 
         wayfire_view view = get_signaled_view(data);
         int view_id = view->get_id();
         prev_view_id = view_id;
 
-        int layout_id = get_layout(view_id);
+        restore_view_layout(view_id, keyboard);
+    };
 
-        wlr_keyboard_notify_modifiers(keyboard,
-                                      keyboard->modifiers.depressed,
-                                      keyboard->modifiers.latched,
-                                      keyboard->modifiers.locked,
-                                      layout_id);
+
+    wf::signal_callback_t on_view_unmapped = [=] (wf::signal_data_t *data) {
+        wayfire_view view = get_signaled_view(data);
+        int view_id = view->get_id();
+        prev_view_id = -1;
+
+        delete_view_layout(view_id);
     };
 
 
@@ -62,13 +78,14 @@ public:
 
     void init() override
     {
-        LOGI("kbdd plugin init");
         output->connect_signal("view-focused", &on_focus_changed);
+        output->connect_signal("view-pre-unmapped", &on_view_unmapped);
     }
 
     void fini() override
     {
         output->disconnect_signal("view-focused", &on_focus_changed);
+        output->disconnect_signal("view-pre-unmapped", &on_view_unmapped);
     }
 
 };
